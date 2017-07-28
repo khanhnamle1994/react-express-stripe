db0ca01892518e90ae37c4daf215bb8acb137778
James Le <le_j6@denison.edu>
2017-07-27 22:57:32 -0700

    Express Stripe Backend

--- frontend/src/constants/server.js
+++ frontend/src/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'http://khanhnamle.com'
+  ? 'http://myapidomain.com'
   : 'http://localhost:8080';
 
 export default PAYMENT_SERVER_URL;
--- frontend/src/constants/stripe.js
+++ frontend/src/constants/stripe.js
@@ -1,5 +1,5 @@
 const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_test_3mTRkAi7SFVJd57TNvkKJMmt'
-  : 'sk_test_1xu7Wxk4EU9Ua1L5x5tF8Ktl';
+  ? 'pk_live_MY_PUBLISHABLE_KEY'
+  : 'pk_test_MY_PUBLISHABLE_KEY';
 
 export default STRIPE_PUBLISHABLE;
